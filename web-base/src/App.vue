22120a1f3bf11160b967c2a5f96c78037becdd22
Cparamal <1224731967@qq.com>
2025-06-12 21:29:38 +0800

    架构搭建

---
+++ web-base/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <!-- 路由视图，显示各个页面 -->
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+/* 全局可以放一些通用样式，若无则留空即可 */
+</style>
